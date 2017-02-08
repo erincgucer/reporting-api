e346cf1c61e7c00b9569411347d112e82233f766
erincgucer <erincgucer@gmail.com>
2017-02-09 00:02:56 +0300

    initial commit

---
+++ src/main/java/com/clearsettle/ReportingApiApplication.java
@@ -0,0 +1,12 @@
+package com.clearsettle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportingApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/clearsettle/ReportingApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.clearsettle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReportingApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
